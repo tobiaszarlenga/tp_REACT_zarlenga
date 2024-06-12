0d8cb5e4f08c687bba54fcf158364ac341ce5945
tobiaszarlenga <162618439+tobiaszarlenga@users.noreply.github.com>
2024-06-12 14:29:59 -0300

    termino el ej2
    termino el ej2

--- react_zarlenga_tps/src/main.jsx
+++ react_zarlenga_tps/src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Ejercicio1 from './components/ejercicio1/Ejercicio1.jsx' 
+import Ejercicio2 from './components/ejercicio2/Ejercicio2.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <Ejercicio1 />
+    {/* <Ejercicio1 /> */}
+    <Ejercicio2 message="world"/>
   </React.StrictMode>,
 )
